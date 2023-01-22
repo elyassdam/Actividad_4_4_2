6abf0f7b92c3530040d1bac5863263192f7d9207
srtyo <srtyo@YASSSINGAMING>
2023-01-22 17:43:38 +0100

    ESta es una actualización

---
+++ src/main/java/com/mycompany/practica/Practica.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practica;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author srtyo
+ */
+public class Practica {
+
+    public static void main(String[] args) {
+        int numero[]=new int[10];
+        double numeros_decimales[]=new double[10];
+        boolean verdadero_falso[]=new boolean[5];
+System.out.println("numeros enteros"+Arrays.toString(numero));        
+System.out.println("numeros decimales"+Arrays.toString(numeros_decimales));        
+System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso)) ;        
+
+    }
+    
+}    

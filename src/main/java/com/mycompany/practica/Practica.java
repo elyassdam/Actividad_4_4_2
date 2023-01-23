9c2d62079aa9fd8d2a9911f11496bfce2aee040e
elyassdam <95480897+elyassdam@users.noreply.github.com>
2023-01-23 22:41:34 +0100

    PracticaCambio.java

--- src/main/java/com/mycompany/practica/Practica.java
+++ src/main/java/com/mycompany/practica/Practica.java
@@ -17,9 +17,12 @@ public class Practica {
         int numero[]=new int[10];
         double numeros_decimales[]=new double[10];
         boolean verdadero_falso[]=new boolean[5];
+        char caracter[]=new char[5];
 System.out.println("numeros enteros"+Arrays.toString(numero));        
 System.out.println("numeros decimales"+Arrays.toString(numeros_decimales));        
-System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso)) ;        
+System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso)) ;   
+      System.out.println("verdadero o falso "+Arrays.toString(caracter)) ;        
+    
 
     }
     
